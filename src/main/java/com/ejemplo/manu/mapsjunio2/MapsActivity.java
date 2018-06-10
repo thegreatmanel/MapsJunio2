fd9ef2afcd4f10c2fcce85b07ca11a500ef15e62
Manu <propanero14@gmail.com>
2018-06-10 19:47:07 +0200

    Creada instancia GoogleApiClient para usar las librerias de la api de Google.

--- src/main/java/com/ejemplo/manu/mapsjunio2/MapsActivity.java
+++ src/main/java/com/ejemplo/manu/mapsjunio2/MapsActivity.java
@@ -2,9 +2,14 @@ package com.ejemplo.manu.mapsjunio2;
 
 import android.graphics.Color;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,9 +18,10 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
     private final LatLng MARCA = new LatLng(42.237023, -8.717944);
     private final LatLng CENTRO = new LatLng(42.237558, -8.717285);
     Location marcaUbicacion =new Location("mi marca");
@@ -31,6 +37,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         marcaUbicacion.setLatitude(MARCA.latitude);
         marcaUbicacion.setLongitude(MARCA.longitude);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
@@ -56,5 +70,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.addCircle(area).setVisible(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CENTRO, 17));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
